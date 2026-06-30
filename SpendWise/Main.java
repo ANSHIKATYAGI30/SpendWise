a04ba0624b3494c5ee235da0447f2a24b7726f06
ANSHIKA TYAGI <86118527+ANSHIKATYAGI30@users.noreply.github.com>
2026-06-30 13:57:14 +0530

    Update Main.java

--- SpendWise/Main.java
+++ SpendWise/Main.java
@@ -2,7 +2,6 @@ import javax.swing.SwingUtilities;
 import view.DashboardFrame;
 
 public class Main {
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {DashboardFrame frame = new DashboardFrame();
         frame.setVisible(true);
